1f176aa94125bdb86290a1f0a296f02d0c88ee7e
Daniel March <daniel_march@sil.org>
2022-06-14 08:43:23 -0400

    Added anonymous name generator and some test code
    see push title

---
+++ AnonName.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupportSiteETL
+{
+    public struct AnonName
+    {
+        public string name;
+        //anything else needed?
+    }
+
+    //stores all generated names and generates new names
+    public class AnonNameGen
+    {
+        private List<AnonName> users;
+        private int randDigitLength; //how many random digits after the "anon"
+
+        public AnonNameGen() //constructor, nothing needed
+        {
+            users = new List<AnonName>();
+            randDigitLength = 6;
+        }
+
+        public AnonName getNewUser()
+        {
+            Random random = new Random(); //for random digits
+            AnonName newUser;
+            newUser.name = "ERROR, MAX USERS!"; //in case max users, this will be returned
+            if (users.Count == Math.Pow(10, randDigitLength)) //max users, no more can be generated
+                return newUser;
+
+            bool valid = false;
+            while(!valid)
+            {
+                //generate a random name like anon274375
+                newUser.name = "anon";
+                for (int i = 0; i < randDigitLength; i++) //add 6 random digits
+                    newUser.name += (random.Next() % 10).ToString(); //random "0" through "9"
+
+                //check to make sure this name doesn't exist
+                valid = true;
+                foreach(AnonName user in users)
+                {
+                    if(user.name == newUser.name)
+                    {
+                        valid = false;
+                        break; //match, not a valid name, redo process
+                    }
+                }
+            }
+            //valid newUser, add to list and return
+            users.Add(newUser);
+            return newUser;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,19 +9,31 @@ Console.WriteLine("Begin Program");
 DiscourseConnection discourseConnection = new DiscourseConnection();
 var discourseUsers = discourseConnection.GetUsers();
 
+/*
 Console.WriteLine("\nAll Discourse Users:");
 foreach(var dUser in discourseUsers) {
     //Console.WriteLine(dUser.ToCSV() + "\n");
     Console.WriteLine(dUser);
 }
-
+*/
 
 QTAConnection q2aConnection = new QTAConnection();
 var q2aUsers = q2aConnection.GetUsers();
 
+/*
 Console.WriteLine("\nAll Q2A Users:");
 foreach(var qUser in q2aUsers) {
     Console.WriteLine(qUser.ToCSV() + "\n");
     //Console.WriteLine(qUser);
 }
-Console.WriteLine("End Program");
+*/
+
+Console.WriteLine("New User Names:");
+AnonNameGen anonGen = new AnonNameGen();
+
+for(int i = 0; i < 101; i++)
+{
+    Console.WriteLine("\t" + anonGen.getNewUser().name);
+}
+
+//Console.WriteLine("End Program");
